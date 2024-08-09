90b593fe3195abf563963775a074c7496f0a8b22
Sherbek <139198851+Sherbek26@users.noreply.github.com>
2024-08-09 23:38:09 +0500

    Initial Comment

--- app/src/main/java/com/sherbek_mamasodiqov/newsharedelementtransition/MainActivity.kt
+++ app/src/main/java/com/sherbek_mamasodiqov/newsharedelementtransition/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -29,10 +30,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewSharedElementTransitionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SharedTransitionScope {
+                    SharedTransitionLayout(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
                         val navController = rememberNavController()
                         NavHost(
-                            modifier = Modifier.padding(innerPadding),
                             navController = navController,
                             startDestination = "listScreen"
                         ) {
